0fc6fcfe92ae8b13bf895dada47f213d0a384a67
DHia' A. SHalabi <hi@dhiashalabi.com>
2025-03-12 09:57:05 +0300

    fix: Remove TypeScript extension from App import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import { FrappeProvider } from './lib'
 import './index.css'
 import { QueryClient } from '@tanstack/react-query'
