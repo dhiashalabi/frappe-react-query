9bc229d7c0132d7789c1b0628bddb33828c0cfed
DHia' A. SHalabi <hi@dhiashalabi.com>
2025-03-13 18:12:26 +0300

    feat: Add React plugin to Vite configuration for improved development experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,5 +20,6 @@ export default defineConfig({
             include: ['src/lib'],
             outDir: 'dist',
         }),
+        react(),
     ],
 })
