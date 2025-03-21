debff04f8619d295ad1fca6dc61784f037cfc518
DHia' A. SHalabi <hi@dhiashalabi.com>
2025-03-21 10:22:26 +0300

    fix: simplify useValidateLink hook by using FrappeDocument and updating query function

--- src/lib/hooks/useValidate.ts
+++ src/lib/hooks/useValidate.ts
@@ -1,7 +1,8 @@
 import { useContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { FrappeContext } from '../context/FrappeContext'
-import type { FrappeConfig, ValidateLinkResponse } from '../types'
+import type { FrappeConfig } from '../types'
+import { FrappeDocument } from '@mussnad/frappe-js-client/dist/frappe/types'
 
 /**
  * Hook to validate a link
@@ -15,19 +16,17 @@ import type { FrappeConfig, ValidateLinkResponse } from '../types'
  *
  * const { data, error, isFetching, mutate } = useValidateLink('DocType', 'name', ['field1', 'field2'])
  */
-export const useValidateLink = <T extends string[]>(doctype: string, docname: string, fields: T) => {
-    const { call } = useContext(FrappeContext) as FrappeConfig
+export const useValidateLink = <T extends FrappeDocument = FrappeDocument>(
+    doctype: string,
+    docname: string,
+    fields: string[],
+) => {
+    const { client } = useContext(FrappeContext) as FrappeConfig
 
-    const query = useQuery<ValidateLinkResponse<T>>({
+    const query = useQuery({
         queryKey: ['validateLink', doctype, docname, fields],
-        queryFn: () => call.get('frappe.client.validate_link', { doctype, docname, fields }),
+        queryFn: () => client.validateLink<T>(doctype, docname, fields),
     })
 
-    return {
-        ...query,
-        data: query.data,
-        error: query.error,
-        isFetching: query.isFetching,
-        mutate: query.refetch,
-    }
+    return query
 }
