a6b6dbec7258f6442e0b31b46badb7fa5b3f6c22
DHia' A. SHalabi <hi@dhiashalabi.com>
2025-03-21 10:36:43 +0300

    feat: add documentation for query string functions in utils to improve code clarity

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -12,6 +12,11 @@ export const getRequestURL = (doctype: string, url: string, docname?: string | n
     return requestURL
 }
 
+/**
+ * Get the query string for the doc list API
+ * @param args - The arguments for the doc list API
+ * @returns The query string for the doc list API
+ */
 export const getDocListQueryString = (args?: GetDocListArgs<FrappeDoc<any>>): string => {
     let queryString = ''
 
@@ -58,6 +63,11 @@ export const getDocListQueryString = (args?: GetDocListArgs<FrappeDoc<any>>): st
     return queryString
 }
 
+/**
+ * Get the query string for the doc count API
+ * @param args - The arguments for the doc count API
+ * @returns The query string for the doc count API
+ */
 export const getDocCountQueryString = (args?: GetCountArgs<any>): string => {
     let queryString = ''
 
@@ -79,6 +89,11 @@ export const getDocCountQueryString = (args?: GetCountArgs<any>): string => {
     return queryString
 }
 
+/**
+ * Encode the query data
+ * @param data - The data to be encoded
+ * @returns The encoded data
+ */
 export const encodeQueryData = (data: Record<string, any>) => {
     const ret = []
     for (const d in data) ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]))
