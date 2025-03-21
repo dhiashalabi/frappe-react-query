b88e850e5ebe484657d284c940aab9df24d21b74
DHia' A. SHalabi <hi@dhiashalabi.com>
2025-03-21 10:35:43 +0300

    fix: simplify query string construction in getDocListQueryString and getDocCountQueryString for improved readability

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -17,42 +17,42 @@ export const getDocListQueryString = (args?: GetDocListArgs<FrappeDoc<any>>): st
 
     /** Fields to be fetched */
     if (args?.fields) {
-        queryString += 'fields=' + JSON.stringify(args?.fields) + '&'
+        queryString += `fields=${JSON.stringify(args?.fields)}&`
     }
 
     /** Filters to be applied - SQL AND operation */
     if (args?.filters) {
-        queryString += 'filters=' + JSON.stringify(args?.filters) + '&'
+        queryString += `filters=${JSON.stringify(args?.filters)}&`
     }
 
     /** Filters to be applied - SQL OR operation */
     if (args?.orFilters) {
-        queryString += 'or_filters=' + JSON.stringify(args?.orFilters) + '&'
+        queryString += `or_filters=${JSON.stringify(args?.orFilters)}&`
     }
 
     /** Fetch from nth document in filtered and sorted list. Used for pagination  */
     if (args?.limit_start) {
-        queryString += 'limit_start=' + JSON.stringify(args?.limit_start) + '&'
+        queryString += `limit_start=${JSON.stringify(args?.limit_start)}&`
     }
 
     /** Number of documents to be fetched. Default is 20  */
     if (args?.limit) {
-        queryString += 'limit=' + JSON.stringify(args?.limit) + '&'
+        queryString += `limit=${JSON.stringify(args?.limit)}&`
     }
 
     if (args?.groupBy) {
-        queryString += 'group_by=' + String(args.groupBy) + '&'
+        queryString += `group_by=${String(args.groupBy)}&`
     }
 
     /** Sort results by field and order  */
     if (args?.orderBy) {
         const orderByString = `${String(args.orderBy?.field)} ${args.orderBy?.order ?? 'asc'}`
-        queryString += 'order_by=' + orderByString + '&'
+        queryString += `order_by=${orderByString}&`
     }
 
     /** Fetch documents as a dictionary */
     if (args?.asDict) {
-        queryString += 'as_dict=' + args.asDict
+        queryString += `as_dict=${args.asDict}`
     }
 
     return queryString
@@ -63,17 +63,17 @@ export const getDocCountQueryString = (args?: GetCountArgs<any>): string => {
 
     /** Filters to be applied - SQL AND operation */
     if (args?.filters) {
-        queryString += 'filters=' + JSON.stringify(args?.filters) + '&'
+        queryString += `filters=${JSON.stringify(args?.filters)}&`
     }
 
     /** Debug mode */
     if (args?.debug) {
-        queryString += 'debug=' + args.debug
+        queryString += `debug=${args.debug}`
     }
 
     /** Cache the result */
     if (args?.cache) {
-        queryString += 'cache=' + args.cache
+        queryString += `cache=${args.cache}`
     }
 
     return queryString
